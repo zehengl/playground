a8243108bf0d6c312137e46dd36a9325cd96d982
Zeheng Li <imzehengl@gmail.com>
2021-01-29 00:05:43 -0700

    fix className

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,11 +3,11 @@ import { version } from "../../package.json";
 
 function Home() {
   return (
-    <section class="hero is-medium is-primary is-bold">
-      <div class="hero-body">
-        <div class="container">
-          <h1 class="title">for ada &amp; dan</h1>
-          <h2 class="subtitle">
+    <section className="hero is-medium is-primary is-bold">
+      <div className="hero-body">
+        <div className="container">
+          <h1 className="title">for ada &amp; dan</h1>
+          <h2 className="subtitle">
             Version <strong>{version}</strong>
           </h2>
         </div>
