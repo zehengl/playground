ea925edce48e0769e386184a850ef11a517c0e5e
Zeheng Li <imzehengl@gmail.com>
2026-07-22 11:56:39 -0600

    Update Home.js

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,7 +5,7 @@ function Home() {
     <section className="hero is-medium is-primary is-bold">
       <div className="hero-body">
         <div className="container">
-          <h1 className="title">for ada &amp; dan</h1>
+          <h1 className="title">for Ada &amp; Dan</h1>
         </div>
       </div>
     </section>
